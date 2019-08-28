35ba13e3610250c8940426fe5c05d1bd4348a83f
McMullen <mabmcmul@iu.edu>
2019-08-28 18:29:38 -0400

    Made changes to E01a assignment

--- main10.py
+++ main10.py
@@ -6,15 +6,15 @@ utils.check_version((3,7))          # make sure we are running at least Python 3
 utils.clear()                       # clear the screen
 
 
-print('Greetings!')
-colors = ['red','orange','yellow','green','blue','violet','purple']
-play_again = ''
+print('Greetings!')                 # prints the statement "Greetings!"
+colors = ['red','orange','yellow','green','blue','violet','purple']   # defines colors. this is a list of options to choose from when asked for input
+play_again = ''                      # play_again is defined as the input
 best_count = sys.maxsize            # the biggest number
-while (play_again != 'n' and play_again != 'no'):
-    match_color = random.choice(colors)
-    count = 0
-    color = ''
-    while (color != match_color):
+while (play_again != 'n' and play_again != 'no'):   # while play_again does not equal the character n, and play_again does not equal "no" means that the program will end when the user says n or no but otherwise, play on!
+    match_color = random.choice(colors)             #random.choice is a command that chooses a random color from the defined list and match_color is the program that the loop will use: match computers random color choice to the input
+    count = 0                                       # count function starts at 0
+    color = ''                                      # color equals input
+    while (color != match_color):                   # As long as the input color doesn't match the random chosen color, rerun program
         color = input("\nWhat is my favorite color? ")  #\n is a special code that adds a new line
         color = color.lower().strip()
         count += 1
